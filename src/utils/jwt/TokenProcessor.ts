d3068a02db3bb613af299c085d053809e3977f1b
Şükrü Tanrıverdi <hugohurleyy@gmail.com>
2024-03-06 20:49:42 +0300

    refactor:jsonwebtoken expiration date

--- src/utils/jwt/TokenProcessor.ts
+++ src/utils/jwt/TokenProcessor.ts
@@ -5,7 +5,7 @@ import UnauthorizedError from "@utils/Error/UnauthorizedError";
 class TokenProcessor {
   generateToken(payload: JwtPayload) {
     return jwt.sign(payload, process.env.JWT_SECRET!, {
-      expiresIn: "30d",
+      expiresIn: "2d",
     });
   }
 
